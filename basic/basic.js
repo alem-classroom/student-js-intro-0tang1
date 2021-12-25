449b43ffd6f3a720e365b5e5ffa2f9ef2ff935bf
Tangzharyk <90594377+0tang1@users.noreply.github.com>
2021-12-25 19:53:46 +0000

    Updated basic

--- basic/basic.js
+++ basic/basic.js
@@ -83,7 +83,7 @@ const fizzbuzz = (N) => {
         if (i % 3 === 0) {
             arr.push("Fizz")
         }
-        else if (i % 5 === 0) {
+        else if (i % 5 === 0 && i % 3 !== 0) {
             arr.push("Buzz")
         } else {
             arr.push(i)
