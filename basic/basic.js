d778080fc6c466e130c937deb0375aaa5081c177
Tangzharyk <90594377+0tang1@users.noreply.github.com>
2021-12-25 19:55:27 +0000

    UPDATED

--- basic/basic.js
+++ basic/basic.js
@@ -80,11 +80,11 @@ const fizzbuzz = (N) => {
     // your code here
     let arr = []
     for (let i = 1; i < N; i++) {
-        if (i % 3 === 0) {
-            arr.push("Fizz")
-        }
-        else if (i % 5 === 0 && i % 3 !== 0) {
+        if (i % 5 === 0 && i % 3 !== 0) {
             arr.push("Buzz")
+        }
+        else if (i % 3 === 0) {
+            arr.push("Fizz")
         } else {
             arr.push(i)
         }
